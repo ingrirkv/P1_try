ba5123c146498a6aa9cf0b24ae1abb32b0f09bfb
ingrirkv <115074945+ingrirkv@users.noreply.github.com>
2022-10-10 23:03:19 +0200

    Initial commit: dette har jeg gjort i dag

--- P1_project.py
+++ P1_project.py
@@ -54,31 +54,63 @@ V_0 =5                 #starting volume in the reservoir given in Mm3
 V_MAX = 10             #maximum volume in the reservoir given in Mm3
 Q_Max = 0.36           #maximum outflow per hour from the reservoir given in Mm3/h
 P_MAX = 100            #maximum production per hour given in MW
-M3S_TO_MM3 = 0.0036    #conversion factor
-E_conv = 0.981         #conversion factor for discharge water to produce electricity
+M3S_TO_MM3 = 0.0036    #conversion factor given in [Mm3/m^3]
+E_conv = 0.981         #conversion factor for discharge water to produce electricity, [Mm3/m^3]
 WV_end = 13000         #end water value for all scenarios given in EUR/Mm3
 rho_s = 0.2            #probability for scenario s, equals 0.2 for all s
 T = 48                 #number of hour
 S = 5                  #number of scenarios
 
-model.V_0 = pyo.Param(within = pyo.NonNegativeIntegers, initialize= 5)
-model.rho_s = pyo.Param(model.s, initialize= 0.2)
+
+#må man bruke param for å bevise/markere de er parametere?
+model.V_0 = pyo.Param(initialize = V_O)
+model.V_MAX = pyo.Param(initialize= V_MAX)
 model.Q_max = pyo.Param(model.t, model.s, initialize=Q_Max)
-model.T = pyo.Param(initialize=48)
+model.P_MAX = pyo.Param(model.t, initialize= P_MAX)
+model.M3S_TO_MM3 = pyo.Param(initialize = M3S_TO_MM3)
+model.E_conv = pyo.Param(initialize = E_conv)
+model.WV_end = pyo.Param(initialize = WV_end)
+model.rho_s = pyo.Param(model.s, initialize= rho_s)
+model.T = pyo.Param(initialize=T)
+model.S = pyo.Param(initialize=S)
+#må set-ene også defineres her som en parameter?
+
 
 
 "Variables"
-model.Z = pyo.Var()     #profitt
-model.P_{t,s} = pyo.Var(model.t, model.s, domain = pyo.NonNegativeReals)                     #produsert elektrisitet [MWh]
-model.I_{t,s} = pyo.Var(model.t,model.s, domain = pyo.NonNegativeReals, initialize = 25*s) #inflow
-model.Q_{t,s} = pyo.Var(model.t,model.s, domain = pyo.NonNegativeReals)                     #outflow
-model.p_{t} = pyo.Var(model.t, model.s, domain = pyo.NonNegativeReals, initialize=50*t) #pris
-model.V_{t,s} = pyo.Var(model.t, model.s, domain = pyo.NonNegativeReals)
+model.z = pyo.Var(within = pyo.NonNegativeReals)     #profitt
+model.P_ts = pyo.Var(model.t, model.s, domain = pyo.NonNegativeReals) #produced electricity#, initialize = E_conv*M3S_TO_MM3)                     #produsert elektrisitet [MWh]
+model.I_ts = pyo.Var(model.t,model.s, domain = pyo.NonNegativeReals) #inflow #, initialize = 25*model.s) #inflow
+model.Q_ts = pyo.Var(model.t,model.s, domain = pyo.NonNegativeReals) #outflow              #outflow
+model.p_t = pyo.Var(model.t, domain = pyo.NonNegativeReals) #price # initialize=50*model.t) #pris
+model.V_ts = pyo.Var(model.t, model.s, domain = pyo.NonNegativeReals) #volume of water
+
+"Functions"
+def price(m,t):
+    pris = (50+model.t)
+    return pris  #vil skrive ut funksjon 50+t = p_ts, og gjenbruke senere i koden
+
+def Inflow(m,t,s):
+    if value(model.t) = 0:
+        return (model.I_ts = 0)
+    elif value(model.t) > 0 && value(model.t) <= 24:
+        return (model.I_ts = 0.18)
+    else
+        return (model.I_ts = 0.09*model.s)
+
+def Production(m,t,s): #må kanskje ta inn Q_ts? tror kanskje ikke denne skal defineres, men legges rett som constraint
+    return(Q_ts * E_conv * 1/M3S_TO_MM3)
 
 "model.OBJ = pyo.Objective(sum())"
-def objective_func(m):
-    return pyo.summation(m.P[t,s], m.p[t], m.ro_scenario[s])
-model.OBJ = pyo.Objective(rule = objective_func)
+def objective_func(m,t,s,T):
+    return pyo.summation(m.P_ts[t,s], m.p_ts[t], m.rho_s[s] for s in model.s for t in model.t) + pyo.summation(WV_end[T,s],V_ts[s] for s in model.s )
+model.OBJ = pyo.Objective(rule = objective_func, sense = pyo.maximize)
+#må vi dobbelsummere over t og s eller holder det å summere en gang slik som her?
+
+def objective_rule (m):
+    return sum(sum(m.obj[s,t] for s in model.s) for t in model.t)
+m.OBJ = pyo.Objective(rule = objective_func, sense = pyo.maximize)
+
 
 def constraints():
     return()
